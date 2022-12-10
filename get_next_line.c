9e11e4f6168a2cf241fc98153d00650fc64c235c
Angeles Rodriguez De Cara <angrodri@c3r2s2.42madrid.com>
2022-12-10 15:28:03 +0100

    first dirty attempt

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: angrodri <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/06 17:37:56 by angrodri          #+#    #+#             */
-/*   Updated: 2022/12/06 19:20:47 by angrodri         ###   ########.fr       */
+/*   Updated: 2022/12/10 15:24:23 by angrodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,28 @@
 
 char	*get_next_line(int fd)
 {
-	char*	line;
-	int		counter;
-	char	str;
+	static char*	line;
+	int				counter;
+	char			str;
 
 	counter = 0;
-	while (read(fd, str, 1) > 0 && str != '\n && str != '\n'')
+	while (read(fd, &str, 1) > 0 && str != '\n')
 	{
 		counter++;
+		write(1, &str, 1);
 	}
+	line = malloc((counter + 1) * sizeof(char));
+	read(fd, line, counter);
+	line[counter + 1] = '\0';
 	// pensar en static variable como string o file descriptor pensando en bonus
+	return(line);
+}
+
+int main(void)
+{
+	int	fd;
+
+	fd = open("foo.txt", O_RDONLY);
+	get_next_line(fd);
+	close(fd);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: angrodri <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/06 18:05:36 by angrodri          #+#    #+#             */
-/*   Updated: 2022/12/06 19:20:52 by angrodri         ###   ########.fr       */
+/*   Updated: 2022/12/10 15:25:55 by angrodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 # include <stdlib.h>
 # include <unistd.h>
+# include <fcntl.h>
 
 char	*get_next_line(int fd);
 
